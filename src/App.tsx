d0e1786995dfece9fd771015c67efefca90c0eee
shafayetullah7 <shafayet769@gmail.com>
2025-08-02 00:03:31 +0600

    initiated

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <div>
+      <h1 class='text-red-500 text-4xl'>Hello World</h1>
+      <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+    </div>
+  );
+};
+
+export default App;
